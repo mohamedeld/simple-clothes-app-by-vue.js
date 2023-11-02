3db891a19891f517292f8f3b06c002ea991f91d8
mohamedeld <mohamed.azoz20010@gmail.com>
2023-11-02 10:51:50 +0200

    initial commit

---
+++ products.js
@@ -0,0 +1,50 @@
+const products = [
+    {
+        id:1,
+        name:"product one",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product1.jpg"
+    },
+    {
+        id:2,
+        name:"product two",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product2.jpg"
+    },
+    {
+        id:3,
+        name:"product three",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product13.jpg"
+    },
+    {
+        id:4,
+        name:"product one",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product1.jpg"
+    },
+    {
+        id:5,
+        name:"product two",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product2.jpg"
+    },
+    {
+        id:6,
+        name:"product three",
+        description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam beatae libero quis adipisci quae maiores tempore quidem veniam possimus laborum molestias, dolorum esse quia ullam placeat soluta doloremque delectus explicabo! Incidunt minima quaerat neque esse architecto doloremque labore iste ipsum?",
+        price:120,
+        instock:5,
+        image:"./images/product13.jpg"
+    }
+];
